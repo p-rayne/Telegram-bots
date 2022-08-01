e43fd9caea3a21d189f6684d909bf805748cd698
p-rayne <marlinaqua@gmail.com>
2022-08-02 00:28:16 +0300

    instead of the env file and the dotenv module, the sh script is now used to start the bot

--- pizza-bot/create_bot.py
+++ pizza-bot/create_bot.py
@@ -1,9 +1,7 @@
 from aiogram import Bot
 from aiogram.dispatcher import Dispatcher
-from dotenv import load_dotenv
 import os
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
-load_dotenv()
 
 storage = MemoryStorage()
 
---
+++ pizza-bot/run_example.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+source ../.venv/bin/activate
+
+export BOT_TOKEN=*your-token
+python3 bot-tg.py
